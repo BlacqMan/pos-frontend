1131058d61c3e4167a89e1fc83d9750b4a77ff25
BlacqMan <donkoremmanuelopoku@gmail.com>
2026-02-04 07:45:10 +0000

    Update AdminUsers.jsx

--- src/admin/pages/AdminUsers.jsx
+++ src/admin/pages/AdminUsers.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "../api/axios";
+import api from "../../api/axios";
 
 const AdminUsers = () => {
   const token = localStorage.getItem("token");
