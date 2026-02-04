99812cea2992410dd9713a18c630b2338e530f82
BlacqMan <donkoremmanuelopoku@gmail.com>
2026-02-04 21:13:39 +0000

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, // keep auth cookies / tokens safe
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "http://localhost:5000/api",
+  withCredentials: true,
 });
 
 export default api;
