86b1dcd1e67903290d873383e22ef3b4fbbba071
BlacqMan <donkoremmanuelopoku@email.com>
2026-02-09 14:57:49 +0000

    updated Axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,4 +7,15 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  }
+);
+
 export default api;
